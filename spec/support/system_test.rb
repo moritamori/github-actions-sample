f9a2af3bbd7063ba5e0b7b09b900c9d52025fea4
moritamori <tamo@sonicgarden.jp>
2020-01-23 01:44:30 +0900

    ciでgoogle chromeインストール+no-sandboxオプションを追加

--- spec/support/system_test.rb
+++ spec/support/system_test.rb
@@ -1,8 +1,9 @@
 RSpec.configure do |config|
   config.before(:each) do |example|
     if example.metadata[:type] == :system
-      driven_by :selenium_chrome_headless, screen_size: [1400, 1400] do |driver_options|
+      driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |driver_options|
         driver_options.add_argument('--disable-dev-shm-usage')
+        driver_options.add_argument('--no-sandbox')
       end
     end
   end
