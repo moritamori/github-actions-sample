950c3145656cd4111def1a452182600a0eed9e51
moritamori <tamo@sonicgarden.jp>
2020-01-23 00:21:48 +0900

    github actionsキャッシュするように

--- spec/support/system_test.rb
+++ spec/support/system_test.rb
@@ -1,7 +1,9 @@
 RSpec.configure do |config|
   config.before(:each) do |example|
     if example.metadata[:type] == :system
-      driven_by :selenium_chrome_headless, screen_size: [1400, 1400]
+      driven_by :selenium_chrome_headless, screen_size: [1400, 1400] do |driver_options|
+        driver_options.add_argument('--disable-dev-shm-usage')
+      end
     end
   end
 end
